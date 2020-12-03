665d151e33846908e9c6956781851940b13287b3
dani7beth <danielledona28@gmail.com>
2020-12-03 14:10:41 -0700

    init commit

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
+
+//state
+let state = {
+  todos = [],
+  errorOccured: false,
+  errorMessage: "",
+  errorStatus: "",
+};
+
 axios
-  .get("http://dummy.restapiexample.com/api/v1/employees")
-  .then(function (response) {
+  .get("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => {
     // handle success
-    console.log(response);
+    console.log(response.data.data);
+    state.errorOccured = false;
+    state.todos = response.data.data;
   })
-  .catch(function (error) {
+  .catch((error) =>{
     // handle error
     console.log(error);
-  })
-  .then(function () {
-    // always executed
   });
+  
+//CHALLENGES
+
+// have a button that triggers a call to get your todos and displays them to your ui 
+//get todos
+const getTodos = () =>{
+  
+};
+//display todos
+
+//have buttons that filter todos that are complete and not complete
+
+//be able to toggle a todo being complete or not (checkbox?)
+
+//be able to sort todos by title and or userID
+
+//add some custom styles css
+
+//add a reset button that doesnt need a new API call to resert todos
