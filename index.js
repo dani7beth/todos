75ed0b4e3b57459acb7663cd6af80a16b7b17661
dani7beth <danielledona28@gmail.com>
2020-12-03 22:44:41 -0700

    filter by complete only gets rid of one incomplete

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 //state
 let state = {
-  todos = [],
+  todos: [],
+  display: false,
   errorOccured: false,
   errorMessage: "",
   errorStatus: "",
@@ -11,9 +12,10 @@ axios
   .get("https://jsonplaceholder.typicode.com/todos")
   .then((response) => {
     // handle success
-    console.log(response.data.data);
+    console.log(response.data);
     state.errorOccured = false;
-    state.todos = response.data.data;
+    state.todos = response.data;
+    render();
   })
   .catch((error) =>{
     // handle error
@@ -25,11 +27,59 @@ axios
 // have a button that triggers a call to get your todos and displays them to your ui 
 //get todos
 const getTodos = () =>{
-  
+  axios
+  .get("https://jsonplaceholder.typicode.com/todos")
+  .then((res)=>{
+    // console.log(res.data.data);
+    state.errorOccured = false;
+    state.todos = res.data;
+    state.todos = false;
+    render();
+  })
+  .catch((err)=>{
+    state.errorOccured = true;
+    state.errorMessage = err.response.data.message;
+    state.errorStatus = err.response.status;
+  });
 };
+
 //display todos
+const renderTodo = (todo) =>{
+  return `<div id='todo'>
+          <h2>${todo.title}</h2>
+          <p>completed: ${todo.completed}</p>
+          </div>`;
+};
+
+const renderTodos = () =>{
+  const {todos, errorOccured, errorMessage, errorStatus} = state;
+  if(errorOccured){
+    return `<div>
+            <h1>Error: ${errorMessage}, failed with status ${errorStatus}</h1>
+            <div onclick='getTodos()'>retry</div>
+            </div>`;
+  }
+  
+  let todosStringArray = todos.map((todo)=>{
+    return renderTodo(todo);
+  });
+  
+
+  
+  let htmlString = `<div id='todos' style='display: none' class='grid'>${todosStringArray.join("")}</div>`;
+
+    
+  return htmlString;
+};
 
 //have buttons that filter todos that are complete and not complete
+const filterComplete = () => {
+  state.todos.filter(todo => {
+    todo.completed === false;
+    todo.display = false;
+  });
+  document.getElementById("todo").style.display = "none";
+};
 
 //be able to toggle a todo being complete or not (checkbox?)
 
@@ -38,3 +88,29 @@ const getTodos = () =>{
 //add some custom styles css
 
 //add a reset button that doesnt need a new API call to resert todos
+
+
+const showDiv = () =>{
+  state.todos.filter(todo => {
+    todo.display === false;
+    document.getElementById("todos").style.display = "block";
+    todo.display = true;
+    console.log(`the display property before: ${todo.display}`);
+    
+  });
+};
+
+const render = () => {
+  let htmlString = `<div>`;
+  htmlString += "<h1>Todos</h1>";
+  htmlString += "<div class='btn-group'>";
+  htmlString += `<button class='btn' onclick='showDiv()'>Get Todos</button>`;
+  htmlString += `<button class='btn' onclick='filterComplete();'>Filter Completed</button>`;
+  htmlString += renderTodos();
+
+  htmlString += "</div>";
+  document.getElementById('app').innerHTML = htmlString;
+};
+
+render();
+console.log("index loaded");
